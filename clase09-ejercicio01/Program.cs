8ca7595d03d4a7c7ad809a74249288d55e63de6a
mmargs <mmargs@github.com>
2022-05-26 00:28:49 -0300

    Agregar ejercicio 1 de clase 9: clase09-ejercicio01

---
+++ clase09-ejercicio01/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Ejercicio 01 - Clase 09
+Curso Intro NET 6.0 - 2022
+Polo Tecnologico Mina Clavero - Incluit
+*/
+
+// Comunicar al usuario el objectivo del programa
+Console.WriteLine("Crear vector con datos aleatorios e invertir.");
+
+// 1) Pedir al usuario la longitud de un vector
+Console.Write("Ingrese longitud de vector: ");
+var longitud = int.Parse(Console.ReadLine());
+
+// 2) Crear el vector del tamaño ingresado.
+int[] vector = new int[longitud];
+
+Random aleatorio = new Random();
+
+Console.WriteLine("Vector: ");
+for (int i = 0; i < vector.Length; i++)
+{
+    // 3) Llenar el mismo con datos aleatorios
+    vector[i] = aleatorio.Next(100);
+    // 4) Mostrar el vector por pantalla
+    Console.Write(vector[i]+" ");
+}
+
+// 5) Invertir el vector, de manera que el primer elemento quede al último y el útimo en el primero.
+for (int i = 0; i < vector.Length/2; i++)
+{
+    var tmp = vector[i];
+    vector[i] = vector[vector.GetUpperBound(0)-i];
+    vector[vector.GetUpperBound(0)-i] = tmp;
+}
+
+// 6) Mostrar el vector nuevamente.
+Console.WriteLine();
+Console.WriteLine("Vector invertido: ");
+foreach(int i in vector)
+{
+    Console.Write(i+" ");
+}
+
+Console.ReadKey();
